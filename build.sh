b3921d6f0b7f2f4225fc31fdd289c61569567403
Christian H <sircanist@gmail.com>
2022-02-08 09:10:57 +0100

    move ontologies out from output into src folder

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t tawny-ctidev . && docker run -v $(pwd)/output:/usr/src/app/output tawny-ctidev
+docker build -t tawny-ctidev . && docker run -v $(pwd):/usr/src/app tawny-ctidev
