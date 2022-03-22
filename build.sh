cedff6c395d17c3db9b30bc4293c35545f1abd21
sircanist <sircanist@gmail.com>
2022-03-22 11:15:18 +0100

    upgrade dependency and decrease memory footprint (owlapi memory leak) add error logging analyse output

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build -t tawny-ctidev . && docker run -v $(pwd):/usr/src/app tawny-ctidev
+
+which docker && docker -v && docker build -t tawny-ctidev . && docker run -v $(pwd):/usr/src/app tawny-ctidev && exit
+which podman && podman -v && podman build -t tawny-ctidev . && podman run -v $(pwd):/usr/src/app tawny-ctidev && exit
